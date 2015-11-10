e4a6f45f5e0937cee9d233641b3fddae6890ef88
Mike O <momokatte@hotmail.com>
2015-11-09 20:50:25 -0800

    Pepe is love Pepe is life

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
+
 # go-drain-example
+
+An example command-line app which reads strings from STDIN or a file, processes them concurrently, and writes 
+them to STDOUT or a file.
+
+This project exists for the purpose of testing, benchmarking, and providing practical working examples for the [https://github.com/momokatte/go-drain](go-drain package).
---
+++ drain-example.go
@@ -0,0 +1,65 @@
+// Copyright 2015 Michael O'Rourke. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+package main
+
+import (
+	"flag"
+	"sync"
+
+	drain "github.com/momokatte/go-drain"
+)
+
+var outStrings chan string
+
+// Example app which reads strings from STDIN or a file, processes them concurrently, and writes them to STDOUT
+// or a file.
+//
+func main() {
+
+	var inFile, outFile string
+	flag.StringVar(&inFile, "infile", "", "Input filename")
+	flag.StringVar(&outFile, "outfile", "", "Output filename")
+	flag.Parse()
+
+	var outWG sync.WaitGroup
+	outStrings = make(chan string, 1024)
+	outWG.Add(1)
+	go func() {
+		if len(outFile) > 0 {
+			for err := drain.ChanToFile(outStrings, outFile); err != nil; {
+				panic(err)
+			}
+		} else {
+			for err := drain.ChanToStdout(outStrings); err != nil; {
+				panic(err)
+			}
+		}
+		outWG.Done()
+	}()
+
+	inStrings := make(chan string, 200)
+	go func() {
+		if len(inFile) > 0 {
+			for err := drain.FileLinesToChan(inFile, inStrings); err != nil; {
+				panic(err)
+			}
+		} else {
+			for err := drain.StdinLinesToChan(inStrings); err != nil; {
+				panic(err)
+			}
+		}
+		close(inStrings)
+	}()
+
+	go func() {
+		for s := range inStrings {
+			// process each string here
+			outStrings <- s
+		}
+		close(outStrings)
+	}()
+
+	outWG.Wait()
+}
